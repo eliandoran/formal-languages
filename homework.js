d9328f4f9940129541397fbee4086a421f1f6faf
Adoris-Elian Doran <adoran@ausy-group.com>
2018-10-10 12:34:13 +0300

    Parse terminal and nonterminal sets partially

--- homework.js
+++ homework.js
@@ -1,5 +1,9 @@
+const expand = require("expand-range");
+
 const separatorSymbol = "$";
 const endingSymbol = "&";
+const terminalSymbols = expand("a..z");
+const nonterminalSymbols = expand("A..Z");
 
 function parseGrammar(input) {
     // Trim ending symbol
@@ -7,7 +11,7 @@ function parseGrammar(input) {
     if (lastChar == endingSymbol) {
         input = input.substring(0, input.length - 1);
     }
-    
+   
     const productions = input
         .split(separatorSymbol)
         .map((production) => {
@@ -17,10 +21,28 @@ function parseGrammar(input) {
             }
         });
 
-    return {
+    const actualTerminalSymbols = new Set(
         productions
+            .map((production) => production.replacement.split(""))            
+            .reduce((acc, symbols) => acc.concat(symbols), [])
+            .filter((symbol) => terminalSymbols.indexOf(symbol) !== -1)
+    );
+
+    const actualNonterminalSymbols = new Set(
+        productions
+            .map((production) => production.initialSymbol)
+            .filter((symbol) => nonterminalSymbols.indexOf(symbol) !== -1)
+    );
+
+    return {
+        productions,        
+        terminalSymbols: actualTerminalSymbols,
+        nonterminalSymbols: actualNonterminalSymbols
     }
 }
+
+const getSetString = (set) =>
+    `{ ${ [...set].join(", ") } }`;
 
 function display(input) {
     var grammar = parseGrammar(input);
@@ -29,7 +51,15 @@ function display(input) {
         .map((production) => `${production.initialSymbol} -> ${production.replacement}`)
         .join("; ");
 
+    let nonterminalSymbolsOutput = 
+        `V_N = ${ getSetString(grammar.nonterminalSymbols) }`;
+
+    let terminalSymbolsOutput =
+        `V_T = ${ getSetString(grammar.terminalSymbols) }`;
+
     console.log(productionsOutput);
+    console.log(nonterminalSymbolsOutput);
+    console.log(terminalSymbolsOutput);
 }
 
 display("SAB$AaA$A@$Ba&");
