3ef85ba888df6748d2ea77e5cab56f40222abfef
Elian Doran <elinnum@gmail.com>
2018-10-10 23:04:07 +0300

    Create prompt for manual data entry

--- js/homework.js
+++ js/homework.js
@@ -55,4 +55,34 @@ function display(input) {
         terminalSymbolsOutput ].join("\n"));
 }
 
-display("SAB$AaA$A@$Ba&");
+// display("SAB$AaA$A@$Ba&");
+
+const inquirer = require("inquirer");
+const prompt = inquirer.createPromptModule();
+
+console.log(`\
+Temă Limbaje formale
+
+Introduceți textul unei gramatici independente de context pentru a afișa mulțimile V_N, V_T, S și P aferente.
+
+Regulile de introducere a datelor sunt următoarele:
+  1.  Primul simbol din prima producție reprezintă axioma (simbolul de start);
+  2.  Simbolul de separare dintre producții este \`$\`.
+  3.  Simbolurile neterminale sunt scrise cu litere mari;
+  4.  Simbolurile terminale sunt scrise cu litere mici;
+  5.  Secvența vidă va fi \`@\`.
+  6.  Simbolul care marchează sfârșitul gramaticii este \`&\`.
+
+Exemplu:
+  SAB$AaA$A@$Ba&
+`
+);
+
+prompt([
+   {
+       name: "grammar",
+       message: "Date gramatică:"
+   } 
+]).then((userData) => {
+    display(userData.grammar);
+});
